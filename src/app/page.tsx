dfa47b17732f0b70e196ee7bc4136d3b4aa4a63b
Johannes Naylor <jonaylor89@gmail.com>
2023-11-07 17:49:20 -0600

    text bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h1>create cover art for your next album</h1>
+      <h1
+        className="text-5xl tracking-tighter pb-10 font-bold text-gray-800" 
+      >
+        create cover art for your next album
+      </h1>
     </div>
   );
 }
