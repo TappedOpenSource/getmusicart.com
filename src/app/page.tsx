9b12f514a0bb25575a17ec9e8bd08b8e629ba146
Johannes Naylor <jonaylor89@gmail.com>
2023-11-19 02:12:09 -0500

    smh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </h1>
       <Link
         href="/login?return_url=/prompting"
-        className='font-bold text-2xl rounded-full bg-blue-500 text-white px-10 py-5 mt-6 transition duration-300 ease-in-out hover:bg-blue-600 mr-6'
+        className='font-bold text-lg rounded-full bg-blue-500 text-white px-10 py-5 mt-6 transition duration-300 ease-in-out hover:bg-blue-600 mr-6'
       >
         get started 
       </Link> 
