f1ea51d6f9ef4cd3b424d6295272cc94fcf696d4
Johannes Naylor <jonaylor89@gmail.com>
2023-11-20 00:32:11 -0500

    try again

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -1,7 +1,7 @@
+import Link from "next/link";
 import SelectableImage from "@/components/SelectableImage";
 
 export default function Selection({
-    params,
     searchParams,
 }: {
     params: { slug: string };
@@ -31,6 +31,15 @@ export default function Selection({
                 <h1 className="text-5xl tracking-tighter pb-10 font-bold">
                     pick your favorite
                 </h1>
+               <div className="w-full md:w-auto flex justify-center">
+                    <Link
+                        href="/prompting" 
+                        className="w-full text-center text-2xl bg-blue-500/10 text-blue-500 hover:text-blue-700 py-4 px-16 rounded-xl"
+                    >
+                        try again
+                    </Link>
+                </div>
+                <div className="h-6" />
                 <div className="grid md:grid-cols-2 gap-4">
                     {
                         selectionImageUris.map((imageUri, index) => (
