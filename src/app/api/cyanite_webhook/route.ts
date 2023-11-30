ccbc79eca053adf42f207cbdda0206b6bd9914eb
Johannes Naylor <jonaylor89@gmail.com>
2023-11-30 18:56:39 -0500

    headers

--- src/app/api/cyanite_webhook/route.ts
+++ src/app/api/cyanite_webhook/route.ts
@@ -1,5 +1,7 @@
 
 export default async function POST(req: Request) {
+    const { headers } = req;
+    console.log({ headers });
     const body = await req.json();
     console.log({ body });
     return new Response('OK', { status: 200 }); 
