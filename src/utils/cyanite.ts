615f1bbc37eaecae7a1570ee415955006cebf317
Johannes Naylor <jonaylor89@gmail.com>
2023-11-30 19:36:19 -0500

    cyanite stuff

---
+++ src/utils/cyanite.ts
@@ -0,0 +1,20 @@
+
+const cyaniteAccessToken = process.env.CYANITE_ACCESS_TOKEN;
+
+export async function uploadTrack() {
+    return;
+}
+
+export async function createLibraryTrack() {
+    return;
+}
+
+export async function enqueueLibraryTrack(libraryTrackId: string) {
+    console.log({ libraryTrackId });
+    return;
+}
+
+export async function enqueueSpotifyTrack(spotifyTrackId: string) {
+    console.log({ spotifyTrackId });
+    return;
+}
